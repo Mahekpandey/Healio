8e678a1bbc3de0ac832d7ceb4fe7037d96261050
Mahek Pandey <mahekpandey0303@gmail.com>
2024-09-27 18:25:27 +0530

    TopDoctors

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
+  const {speciality} = useParams()
+  const {doctors} = useContext(AppContext)
+  const [filterDoc,setFilterDoc] = useState([])
   return (
     <div>
-      
+      <p>Browse through the doctors specialist</p>
+      <div>
+        <div>
+          <p>General physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
+        <div>
+          {
+            filterDoc.map((item, index) => (
+              <div onClick={() => (navigate(`/appointment/${item._id}`))} className='border boarder-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                <img className='bg-blue-50' src={item.image} alt='' />
+                <div className='p-4'>
+                  <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                    <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                  </div>
+                  <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                  <p className='text-gray-600 text-sm'>{item.speciality}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
