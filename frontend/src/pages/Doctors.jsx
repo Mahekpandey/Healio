b2531de9766d864f976fa099ab3840dfdc8cabf8
Mahek Pandey <mahekpandey0303@gmail.com>
2024-09-28 08:41:12 +0530

    Filter Doc

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,13 +1,28 @@
-import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
 const Doctors = () => {
   const {speciality} = useParams()
   const {doctors} = useContext(AppContext)
-  const [filterDoc,setFilterDoc] = useState([])
+  const [filterDoc, setFilterDoc] = useState([]) // Added initial state
+  const navigate = useNavigate()
+
+  const applyFilter = () => {
+    if(speciality){
+      setFilterDoc(doctors.filter(doc => doc.speciality === speciality))
+    } else {
+      setFilterDoc(doctors)
+    }
+  }
+
+  useEffect(() => {
+    console.log('doctors: ', doctors); // Check if doctors is populated
+    applyFilter()
+  }, [doctors, speciality])
+
   return (
-    <div>
+    <div className>
       <p>Browse through the doctors specialist</p>
       <div>
         <div>
@@ -18,20 +33,20 @@ const Doctors = () => {
           <p>Neurologist</p>
           <p>Gastroenterologist</p>
         </div>
-        <div>
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
           {
-            filterDoc.map((item, index) => (
-              <div onClick={() => (navigate(`/appointment/${item._id}`))} className='border boarder-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+            filterDoc.length > 0 ? filterDoc.map((item, index) => (
+              <div onClick={() => navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                 <img className='bg-blue-50' src={item.image} alt='' />
                 <div className='p-4'>
-                  <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                  <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                     <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
                   </div>
                   <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                   <p className='text-gray-600 text-sm'>{item.speciality}</p>
                 </div>
               </div>
-            ))
+            )) : <p>No doctors available</p>
           }
         </div>
       </div>
